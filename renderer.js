c284b54deaebefc82aff7a23073f28a2ac3820f1
vikash <>
2018-05-11 16:59:45 +0530

    updated

--- renderer.js
+++ renderer.js
@@ -7,8 +7,19 @@ const { createJabraApplication } = require('jabra-dev');
 
 const jabra = createJabraApplication('YhufvN4CVGhrs2A8NbgXGyDQ1bjCkXsqrcVNuXimm2c=');
 
-jabra.on('attach', () =>{
+jabra.on('attach', (device) =>{
     console.log('device attached');
+    device.lock();
+    device.getSettingAsync()
+        .then((settings) => {
+            console.log('got settings..', settings)
+        })
+        .catch((err) => {
+            console.log('error while getting settings', err);
+        })
+        setInterval(() => {
+            console.log('keep going... at every 1sec');
+        }, 1000)
 })
 
 jabra.on('detach', () => {
